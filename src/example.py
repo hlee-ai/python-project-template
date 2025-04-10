ec79058eb540d9e882cc3245db06acf24a7f2316
이호성 <hlee24@snu.ac.kr>
2025-04-10 17:58:25 +0900

    feat: implement hello function

---
+++ src/example.py
@@ -0,0 +1,19 @@
+"""
+Example Dummy Python Script
+
+This script contains a simple function that returns a greeting message.
+"""
+
+
+def hello(name: str) -> str:
+    """
+    Example function that returns a greeting message with the provided name.
+
+    Args:
+        name (str): The name to include in the greeting.
+
+    Returns:
+        str: A greeting message.
+
+    """
+    return f"Hello, {name}!"
